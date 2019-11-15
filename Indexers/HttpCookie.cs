39158d0be8ee19e7ac9b3c137f784317d430afe7
Burak Kocak <burakkocak884@hotmail.com>
2019-11-15 16:10:28 -0500

    indexers

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private Dictionary<string, string> _dictionary;
+        public DateTime Expiry {get; set;}
+        public HttpCookie()
+        {
+            _dictionary  = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get {return _dictionary[key];}
+            set { _dictionary[key] = value;}
+        }
+    }
+}
