f78ea59a2953f70c583625d5ded53113855bd8b0
Burak Kocak <burakkocak884@hotmail.com>
2019-11-15 15:15:12 -0500

    fileds exercise

---
+++ Fields/Customer.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+namespace Fields
+{
+public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders = new List<Order>();
+
+        public Customer()
+        {
+         
+
+        }
+
+        public Customer(int id)
+        {
+
+        }
+
+         public Customer(int id, string name)
+         :this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // Orders = new List<Order>();
+        }
+    }
+}
